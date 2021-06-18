278ec9a44c80022f35761b8147ce10c79663987f
Alys Brooks <alys@gaiwan.co>
2021-06-18 01:06:22 -0500

    Tidy source.

--- src/lioss/github.clj
+++ src/lioss/github.clj
@@ -5,24 +5,22 @@
             [lioss.git :as git]
             [clojure.java.shell  :as shell]
             [clojure.string :as string]) (:import [java.time LocalDateTime Instant ]
-           [java.time.temporal ChronoField])
-  )
+                                                  [java.time.temporal ChronoUnit ChronoField]))
 
-
-(def highlighted 
- "Repositories that we have highlighted in the README of this repository." 
+(def highlighted
+  "Repositories that we have highlighted in the README of this repository."
   #{"kaocha" "deep-diff2" "regal" "uri" "glogi" "ansi" "chui"
-                   "funnel" "edn-lines" "fetch" "data-printers" "daedalus"
-                   "cljbox2d" "spec-monstah-malli" "trikl" "zipper-viz"})
+    "funnel" "edn-lines" "fetch" "data-printers" "daedalus"
+    "cljbox2d" "spec-monstah-malli" "trikl" "zipper-viz"})
 
 (defn get-next-url
   "Gets the next URL from the Link header."
   [header]
   (first (for [header-line (string/split header #",")
-          :let [[url rel] (string/split header-line #";")
-                string (second (re-matches #"<(.*)>" (string/trim url)))] 
-          :when (string/includes? rel "next")]
-    string)))
+               :let [[url rel] (string/split header-line #";")
+                     string (second (re-matches #"<(.*)>" (string/trim url)))]
+               :when (string/includes? rel "next")]
+           string)))
 
 (defn get-all-lioss-repositories
   "Fetch every Lambda Island Open Source repository."
@@ -36,49 +34,39 @@
         (recur next-url new-repositories)
         new-repositories))))
 
-
-(defn get-top-lioss-repositories 
+(defn get-top-lioss-repositories
   "Fetches only the Lambda Island Open Source repositories featured in our README."
   []
   (filter  #(contains? highlighted  (get %  "name" ))
-    (get-all-lioss-repositories)))
-(
-
-  
-  
-
-(filter #(> ()) (map #(LocalDateTime/parse (apply str (butlast (get % "updated_at")))) l))
-  )
-
+          (get-all-lioss-repositories)))
 
 (defn get-recent-lioss-repositories
   "Gets Lambda Island Open Source repositories modified after a specific date.
-  
+
   If no date is specified, assumes 365 days ago."
   ([]
    (let [year-ago (.minus (Instant/now) 365 ChronoUnit/DAYS) ]))
   ([start-date]
-   (filter 
+   (filter
      #(= -1 (compare start-date (Instant/parse (get % "updated_at")) ))
-     (get-all-lioss-repositories)))) 
+     (get-all-lioss-repositories))))
 
 (defn decode-base64 [s]
-  (:out (shell/sh "base64" "-d" :in s))
-  )
+  (:out (shell/sh "base64" "-d" :in s)))
 
 (defn get-file
   "Gets a file from the respository"
   [repository path request-opts]
   (-> (get  repository "contents_url")
       (string/replace "{+path}" path)
-       (http/get request-opts)
+      (http/get request-opts)
       deref
       :body
       json/decode
       (get "content")
       decode-base64))
 
-(def get-token 
+(def get-token
   (memoize (fn []
              (println "GitHub token needed for this operation.")
              (println "You can create a token by visiting https://github.com/settings/tokens.")
@@ -87,24 +75,23 @@
 
 (defn get-clojars-lioss-repositories
   "Gets Lambda Island Open Source repositories with a Clojars badge.
-  
+
   This is a pretty accurate way of finding actual libraries or applications,
   rather than repositories that are merely for demonstration purposes.
-  
+
   Because it does a signficant number of requests, it requires authorization."
   []
   (let [headers {"Authorization" (str "token " (get-token))} ]
-    (filter #(string/includes? (get-file %  "README.md" {:headers headers}) 
+    (filter #(string/includes? (get-file %  "README.md" {:headers headers})
                                "img.shields.io/clojars")
             (get-all-lioss-repositories))))
 
-
-
-(defn clone-repositories [respositories dir] 
+(defn clone-repositories [repositories dir]
   (doseq
     [url  (map #(get % "clone_url") repositories)]
-    (try 
+    (try
       (git/clone-with-cwd! url dir )
       (catch Exception _e
         (println _e)))))
 
+(clone-repositories (get-clojars-lioss-repositories) "..")
