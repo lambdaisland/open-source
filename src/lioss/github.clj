4cf96546a83cc7eed7893ae93d87c38d8858e34c
Alys Brooks <alys@gaiwan.co>
2021-06-29 11:03:40 -0500

    Add REPL namespace.

--- src/lioss/github.clj
+++ src/lioss/github.clj
@@ -1,4 +1,3 @@
-
 (ns lioss.github
   (:require [org.httpkit.client :as http]
             [cheshire.core :as json]
@@ -33,12 +32,15 @@
       Integer/parseInt
       zero?))
 
+
 (defn get-all-lioss-repositories
   "Fetch every Lambda Island Open Source repository."
-  []
+  [& token]
   (loop [url "https://api.github.com/orgs/lambdaisland/repos"
          repositories []]
-    (let [response @(http/get url)
+    (let [headers (when-not (nil? token) 
+                    {"Authorization" (str "token " (get-token))})
+          response @(http/get url headers)
           body (json/decode (:body response))
           new-repositories (into repositories body)]
       (if-let [next-url  (get-next-url (get-in response [:headers :link]))]
@@ -91,8 +93,8 @@
   rather than repositories that are merely for demonstration purposes.
 
   Because it does a signficant number of requests, it requires authorization."
-  []
-  (let [headers {"Authorization" (str "token " (get-token))} ]
+  [token]
+  (let [headers {"Authorization" (str "token " token)} ]
     (filter #(string/includes? (get-file %  "README.md" {:headers headers})
                                "img.shields.io/clojars")
             (get-all-lioss-repositories))))
@@ -114,16 +116,31 @@
 
 (defn get-all-repository-issues
   "Fetch all issues in a Lambda Island Open Source repository."
-  [repository]
+  [repository token]
   (loop [url (format "https://api.github.com/repos/lambdaisland/%s/issues" repository)
          issues []]
-    (let [headers {"Authorization" (str "token " (get-token))}
+    (let [headers {"Authorization" (str "token " token)}
           response @(http/get url {:headers headers})
           body (json/decode (:body response))
           new-issues (into issues body)]
-      (if-let [next-url  (some-> response
-                                 (get-in [:headers :link])
-                                 get-next-url)]
+      (if-let [next-url (some-> response
+                                (get-in [:headers :link])
+                                get-next-url)]
         (recur next-url new-issues)
         new-issues))))
 
+(comment (->> (get-all-repository-issues "kaocha")
+              (sort-by #(get % "updated_at"))
+              reverse
+              ;; (map #(get % "title"))
+              (map (fn [{:strs [title updated_at] }] [title updated_at] ))))
+
+(comment (def issues 
+  (->> (map #(get % "name") (get-clojars-lioss-repositories (get-token)))
+       (mapcat #(do (prn %) (get-all-repository-issues %) ) )
+       (sort-by #(get % "updated_at"))
+       reverse
+       ;; (map #(get % "title"))
+       (map (fn [{:strs [title updated_at] }] [title updated_at] )))))
+
+(comment (clone-repositories (get-clojars-lioss-repositories) ".."))
---
+++ src/lioss/repl.clj
@@ -0,0 +1,19 @@
+(ns lioss.repl
+   (:require [lioss.github :as github]) 
+  )
+
+
+(defn get-repository-issues
+  "Fetch all issues in a Lambda Island Open Source repository."
+  [repository]
+
+  (github/get-all-repository-issues repository (github/get-token)))
+
+
+(defn get-all-clojars-repository-issues
+  "Fetch all issues in a Lambda Island Open Source repository, 
+  asking for a token."
+  []
+  (->> (github/get-clojars-lioss-repositories (github/get-token))
+
+       (pmap github/get-all-repository-issues)))
